6e6c2a72f164e6a322582f023f84c30f88591e3e
flutterd3 <107663176+flutterd3@users.noreply.github.com>
2022-07-25 09:08:43 +0545

    hi

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,15 @@
+import 'package:amazon_clone/widgets/search_bar_widget.dart';
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: SearchBarWidget(isReadOnly: false, hasBackButton: true),
+      ),
+    );
+  }
+}
